fa75785fb2bdf17fad2cec49704ea648dcfc171b
kagan.meric <mustafakaganmeric@gmail.com>
2023-07-06 02:10:40 +0300

    update main

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"github.com/gofiber/fiber/v2"
 	"loggerservice/pkg/config"
+	"loggerservice/pkg/controller"
 	"loggerservice/pkg/data"
+	"loggerservice/pkg/utils"
 )
 
 func main() {
 
 	// Initialize environment variables
 	config.InitEnvConfigs()
+	// Initialize database source name
+	data.InitDSN()
 
-	client := data.InitDB()
-	defer data.CloseDB(client)
+	app := fiber.New(fiber.Config{
+		//ReadTimeout:   time.Second * 15,
+		//WriteTimeout:  time.Second * 15,
+		Concurrency:  10,
+		ServerHeader: "logger_service_v1",
+		AppName:      "logger_service_v1",
+	})
+
+	// Register routes
+	controller.RegisterRoutes(app)
+
+	// Listen on port
+	err := app.Listen(config.EnvConfigs.LocalServerPort)
+	utils.FatalErr("Error while serving the api", err)
 
 }
