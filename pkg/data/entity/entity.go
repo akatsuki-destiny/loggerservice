66f443a6299604e531c5ffc729591a8925a384f0
kagan.meric <mustafakaganmeric@gmail.com>
2023-07-11 14:42:39 +0300

    update

--- pkg/data/entity/entity.go
+++ pkg/data/entity/entity.go
@@ -6,16 +6,12 @@ import (
 )
 
 type ErrLog struct {
-	ID             primitive.ObjectID `bson:"_id,omitempty"`
-	Level          string             `bson:"level"`
-	Source         string             `bson:"source"`
-	Message        string             `bson:"message"`
-	Error          string             `bson:"error"`
-	Request        string             `bson:"request"`
-	RequestHeader  string             `bson:"request_header"`
-	Response       string             `bson:"response"`
-	ResponseHeader string             `bson:"response_header"`
-	Timestamp      time.Time          `bson:"timestamp"`
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Level     string             `bson:"level"`
+	Source    string             `bson:"source"`
+	Message   string             `bson:"message"`
+	Error     string             `bson:"error"`
+	Timestamp time.Time          `bson:"timestamp"`
 }
 
 type SuccessLog struct {
